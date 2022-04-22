eb1b2c01dd05e3807e088371d02e2ebab2a224f0
InfantRaju-git <infantraju123@gmail.com>
2022-04-22 10:03:06 +0530

    Update backupdata.sh

--- backupdata.sh
+++ backupdata.sh
@@ -1,2 +1,3 @@
+# to take backup and store in the date when the backup is taken
 docker exec mysqldoc /usr/bin/mysqldump -u root --password=root masterkubedb > "$(date +"%Y_%m_%d_%I_%M_%p").sql"
 cp "$(date +"%Y_%m_%d_%I_%M_%p").sql" Dump-Files/
