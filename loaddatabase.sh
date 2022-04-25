741aae2b7f0e51ebdc944ff5c88df28cd5f3193f
InfantRaju-git <infantraju123@gmail.com>
2022-04-25 14:32:21 +0530

    Update loaddatabase.sh

--- loaddatabase.sh
+++ loaddatabase.sh
@@ -12,23 +12,18 @@ git pull
 
 mysqlip=$(docker inspect mysqldoc --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}')
 apt install mysql-client
-mysql -uroot -proot -h $mysqlip
-drop database masterkubedb;
-create database masterkubedb;
-source $1;
-exit
+docker exec mysqldoc bash -c "mysql -uroot -proot -h $mysqlip; drop database masterkubedb; create database masterkubedb; source $1;"
 
-#cd ..
 
-docker exec -it axon3 bash
-rm webapps/Axon/WEB-INF/classes/ActualDetails.properties
-rm webapps/Axon/WEB-INF/classes/ApprovedDetails.xml
-rm webapps/Axon/WEB-INF/classes/mkpubkey
-rm webapps/Axon/WEB-INF/classes/sig
-cp ActualDetails.properties webapps/Axon/WEB-INF/classes/
-cp ApprovedDetails.xml webapps/Axon/WEB-INF/classes/
-cp mkpubkey webapps/Axon/WEB-INF/classes/
-cp sig webapps/Axon/WEB-INF/classes/
-exit
+docker exec axon3 bash -c "
+rm webapps/Axon/WEB-INF/classes/ActualDetails.properties;
+rm webapps/Axon/WEB-INF/classes/ApprovedDetails.xml;
+rm webapps/Axon/WEB-INF/classes/mkpubkey;
+rm webapps/Axon/WEB-INF/classes/sig;
+cp ActualDetails.properties webapps/Axon/WEB-INF/classes/;
+cp ApprovedDetails.xml webapps/Axon/WEB-INF/classes/;
+cp mkpubkey webapps/Axon/WEB-INF/classes/;
+cp sig webapps/Axon/WEB-INF/classes/;
+"
 
 echo "Loaded Data to the Axon"
