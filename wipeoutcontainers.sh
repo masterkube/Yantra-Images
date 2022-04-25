71430060d2df48ab6b8caf1280182409a4493c54
InfantRaju-git <infantraju123@gmail.com>
2022-04-25 14:52:19 +0530

    Update wipeoutcontainers.sh

--- wipeoutcontainers.sh
+++ wipeoutcontainers.sh
@@ -1,4 +1,5 @@
 docker system prune -a --force
+docker network rm axon-net
 docker container stop $(docker container ls -aq)
 docker container rm $(docker container ls -aq)
 docker rmi $(docker image ls)
