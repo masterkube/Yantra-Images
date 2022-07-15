a7edd8b2325c946527d4d7f4a77a554663356990
JagzZ96 <89344211+JagzZ96@users.noreply.github.com>
2022-07-15 10:30:02 +0530

    Update installdocker.sh

--- installdocker.sh
+++ installdocker.sh
@@ -1,7 +1,7 @@
 #install docker
 sudo apt-get remove docker.io
-mkdir /var/lib/docker/containers/
 sudo apt-get update
 sudo apt install docker.io
 sudo snap install docker
 docker --version
+mkdir /var/lib/docker/containers/
