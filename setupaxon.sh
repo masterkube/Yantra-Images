93a820da54b88b05ee17efc289da686e36f83d66
InfantRaju-git <infantraju123@gmail.com>
2022-09-02 21:31:54 +0530

    Update setupaxon.sh

--- setupaxon.sh
+++ setupaxon.sh
@@ -5,4 +5,4 @@ docker run -d --net axon-net --name mysqldoc masterkube/axonmysql:3.0
 docker run -d -p 3000:8080 --net axon-net --link mysqldoc --name axon3 masterkube/axon:3.0
 docker run -d --name console -p 80:80 masterkube/console:2.0
 docker run -d --name voice -p 90:80 -p 92:443 masterkube/axonvoice:1.0
-docker run -d --name axonapi -p 8000:8080 masterkube/axonapi:1.0
+docker run -d -p 8000:8080 --net axon-net --link mysqldoc --name axonapi masterkube/axonapi:1.0
